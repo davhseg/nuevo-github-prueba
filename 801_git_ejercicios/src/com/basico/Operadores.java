d47d41540869c40fd967b7eaeefa394bbd10bc0b
David <davidhseguridad@gmail.com>
2019-06-19 09:50:40 +0200

    cambios de numeros

---
+++ 801_git_ejercicios/src/com/basico/Operadores.java
@@ -0,0 +1,15 @@
+package com.basico;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+	int n1 = 100;
+	int n2=50;
+	System.out.println ("La suma es: "+ (n1+n2));
+	System.out.println ("La resta es: "+ (n1-n2));
+	System.out.println ("La división es: "+ (n1/n2));
+	System.out.println ("La multiplicación es: "+ (n1*n2));
+	System.out.println ("EL módulo es: "+ (n1%n2));
+	}
+
+}
